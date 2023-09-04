a51098b54e4cf77b619fa2a5f7dfbc219b5c5338
adouiyeh <adouiyeh@e2r13p7.1337.ma>
2023-09-04 15:08:40 +0100

    C06 ex03

--- C06/ex03/ft_sort_params.c
+++ C06/ex03/ft_sort_params.c
@@ -6,32 +6,61 @@
 /*   By: adouiyeh <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/04 03:10:31 by adouiyeh          #+#    #+#             */
-/*   Updated: 2023/09/04 03:41:47 by adouiyeh         ###   ########.fr       */
+/*   Updated: 2023/09/04 15:06:09 by adouiyeh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <unistd.h>
 
 int	ft_strcmp(char *s1, char *s2)
 {
 	int	i;
 
 	i = 0;
-	while (s1[i] && s2[i])
+	while (s1[i] && s1[i] == s2[i])
+		i++;
+	return (s1[i] - s2[i]);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
 	{
-		if (s1[i] > s2[i])
-			return (1);
+		write(1, &str[i], 1);
 		i++;
 	}
-	return (0);
+	write(1, "\n", 1);
+	return ;
 }
 
 int	main(int ac, char **av)
 {
-	int	i;
-	int	j;
-	
-	i = 0;
-	while ()
+	int		i;
+	char	*swp;
+
+	i = 1;
+	while (i < ac)
 	{
-		
+		while (i < ac - 1)
+		{
+			if (ft_strcmp(av[i], av[i + 1]) > 0)
+			{
+				swp = av[i + 1];
+				av[i + 1] = av[i];
+				av[i] = swp;
+			}
+			i++;
+		}
+		i = 1;
+		ac--;
+	}
+	i = 1;
+	while (av[i])
+	{
+		ft_putstr(av[i]);
+		i++;
 	}
 }
